db23c0a37a6963be3adc721db094fb04f068f7ae
Nathan Pugsley <pugsleyn@uni.coventry.ac.uk>
2017-10-17 18:40:42 +0100

    Radio boxes that alert a value. Can't get it to do what I want, so will be using range slider moving forward

---
+++ Website/js/script.js
@@ -0,0 +1,73 @@
+function q1() 
+{
+    var type = document.getElementsByName("q1");
+    if(type[0].checked)
+    {
+        var a1 = type[0].value;
+        alert(a1);
+    }
+                    
+    else if(type[1].checked)
+    {
+        var a2 = type[1].value;
+        alert(a2);
+    }
+                    
+    else if(type[2].checked)
+    {
+        var a3 = type[2].value;
+        alert(a3);
+    }
+                    
+    else if(type[3].checked)
+    {
+        var a4 = type[3].value;
+        alert(a5);
+    }
+                    
+    else if(type[4].checked)
+    {
+        var a5 = type[4].value;
+        alert(a5);
+    }
+    
+    
+    var test = 
+}
+
+
+
+//function q2() 
+//{
+//    var type = document.getElementsByName("q2");
+//    if(type[0].checked)
+//    {
+//        var val = type[0].value;
+//        alert(val);
+//    }
+//                    
+//    else if(type[1].checked)
+//    {
+//        var val = type[1].value;
+//        alert(val);
+//    }
+//                    
+//    else if(type[2].checked)
+//    {
+//        var val = type[2].value;
+//        alert(val);
+//    }
+//                    
+//    else if(type[3].checked)
+//    {
+//        var val = type[3].value;
+//        alert(val);
+//    }
+//                    
+//    else if(type[4].checked)
+//    {
+//        var val = type[4].value;
+//        alert(val);
+//    }
+//}
+//                

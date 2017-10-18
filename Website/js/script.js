6362290e9ed9e74fecae3de60d842ee1bdfb6454
Nathan Pugsley <pugsleyn@uni.coventry.ac.uk>
2017-10-18 02:32:13 +0100

    Got the basic quiz working. Different questions that are answered with a slider. Depending what you answer will reveal a different comment when a button is pressed. Potential next step is to only display one question on screen at a time.

--- Website/js/script.js
+++ Website/js/script.js
@@ -18,3 +18,25 @@ function myFunction2() {
     document.getElementById("demo").innerHTML = finalPoints;
 }
 
+function myFunction3() {
+    var s1 = $('#sliderOne').val();
+    var s2 = $('#sliderTwo').val();
+    var s3 = $('#sliderThree').val();
+    var s4 = $('#sliderFour').val();
+    var s5 = $('#sliderFive').val();
+    
+    var f = parseInt(s1) + parseInt(s2) + parseInt(s3) + parseInt(s4) + parseInt(s5);
+    
+    if (f >= 0 && f <100) {
+        document.getElementById("demo").innerHTML = "Lowest score";
+    } else if (f >= 100 && f <200) {
+        document.getElementById("demo").innerHTML = "Low score";
+    } else if (f >= 200 && f <300) {
+        document.getElementById("demo").innerHTML = "Medium score";
+    } else if (f >= 300 && f <400) {
+        document.getElementById("demo").innerHTML = "High score";
+    } else if (f >= 400) {
+        document.getElementById("demo").innerHTML = "Highest score";
+    }  
+   
+}

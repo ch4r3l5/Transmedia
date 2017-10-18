53c700c90d071fdf7a399b41a561a8c3308a5208
Nathan Pugsley <pugsleyn@uni.coventry.ac.uk>
2017-10-18 01:34:01 +0100

    Added five questions, all answers add to make a final result. Will next make the result display a different comment, dpending what range it lays between

--- Website/js/script.js
+++ Website/js/script.js
@@ -1,7 +1,20 @@
 function myFunction() {
-    var x = document.getElementById("myRange");
+    var x = document.getElementById("sliderOne");
     var currentVal = x.value;
     
         document.getElementById("demo").innerHTML = "Value: " + currentVal;
     
 }
+
+function myFunction2() {
+    var s1 = $('#sliderOne').val();
+    var s2 = $('#sliderTwo').val();
+    var s3 = $('#sliderThree').val();
+    var s4 = $('#sliderFour').val();
+    var s5 = $('#sliderFive').val();
+    
+    var finalPoints = parseInt(s1) + parseInt(s2) + parseInt(s3) + parseInt(s4) + parseInt(s5);
+    
+    document.getElementById("demo").innerHTML = finalPoints;
+}
+
